24b0257fb61708c889329599c2d716181a49fc3a
Sudiksha Pradhan <sudiksha016@gmail.com>
2020-06-26 01:38:05 -0500

    finished hyperlinks - commit :)

--- PiquantApp/ViewController.swift
+++ PiquantApp/ViewController.swift
@@ -10,18 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-  
-           
-          
-        
-        // enchiladas.addTarget(self, action: #selector(openLink), for: .touchUpInside)
-           
-    
-    
-    
-    
-   
-    
    @IBAction func enchiladas(_ sender: Any) {
         UIApplication.shared.open(URL(string: "https://www.gimmesomeoven.com/best-chicken-enchiladas-ever/")! as URL, options: [:], completionHandler: nil)
     }
@@ -32,6 +20,157 @@ class ViewController: UIViewController {
         UIApplication.shared.open(URL(string: "http://www.withsaltandpepper.com/2017/01/04/sesame-tuna-wraps/")! as URL, options: [:], completionHandler: nil)
         
     }
+    
+    @IBAction func stuffedShells(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "http://www.naturalcomfortkitchen.com/protein-power-beans-greens-stuffed-shells/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func chickenMeatBalls(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://tastythin.com/chicken-parmesan-meatballs/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func quinoaCasserole(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.asweetpeachef.com/healthy-quinoa-make-ahead-casserole/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func fusili(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.bonappetit.com/recipe/fusilli-alla-vodka-basil-parmesan")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func curriedCauliflower(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.heynutritionlady.com/curried-cauliflower-chickpea-wraps/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func chipotlePortobello(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.feastingathome.com/chipotle-portobello-tacos/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func veganAlfredo(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.feastingathome.com/vegan-alfredo-sauce/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func chineseEggplant(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.feastingathome.com/chinese-eggplant/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func misoBurger(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.feastingathome.com/vegan-portobello-mushroom-burger/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func dreamyIceCream(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.thekitchn.com/how-to-make-vegan-ice-cream-cooking-lessons-from-the-kitchn-204755")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func soup(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.foodnetwork.com/recipes/ellie-krieger/tuscan-vegetable-soup-recipe-1957503")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func potPie(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.foodnetwork.com/recipes/aida-mollenkamp/vegetarian-pot-pie-recipe-1945008")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func grilledVeg(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.foodnetwork.com/recipes/giada-de-laurentiis/grilled-vegetables-recipe-1942208")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func buttermilk(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-buttermilk-waffles-7449241")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func bakedMac(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.foodnetwork.com/recipes/alton-brown/baked-macaroni-and-cheese-recipe-1939524")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func penneTomato(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.foodnetwork.com/recipes/giada-de-laurentiis/penne-with-sun-dried-tomato-pesto-recipe-1942250")! as URL, options: [:], completionHandler: nil)
+    }
+    
+   
+    @IBAction func macAndCheese(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://therealfoodrds.com/dairy-free-mac-and-cheese/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func keyLime(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.godairyfree.org/recipes/mini-frozen-key-lime-tarts")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func cupcakes(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.haagendazs.us/recipes/chocolate-ice-cream-cupcakes")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func bread(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.lactaid.com/recipes/classic-white-bread")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func pizza(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.lactaid.com/recipes/lactose-free-white-pizza")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func mashedPotato(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.tasteslovely.com/dairy-free-mashed-potatoes/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func gingerTofu(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://thefigtreeblog.com/2015/11/ginger-orange-stirfry-with-tofu.html")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func pastaNoGluten(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.delish.com/cooking/recipe-ideas/a55824/best-gluten-free-pasta-recipe/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func peppersAndPilaf(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.bbcgoodfood.com/recipes/spice-crusted-aubergines-peppers-pilaf")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func balsamicGoat(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.bbcgoodfood.com/recipes/balsamic-shallots-carrots-goats-cheese")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func bbqMeatBalls(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.bbcgoodfood.com/recipes/barbecue-meatball-kebabs")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func glutenFreePancakes(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.bbcgoodfood.com/recipes/gluten-free-pancakes")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func babka(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "http://www.eatingwell.com/recipe/280593/cinnamon-raisin-babka/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func flank(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "http://www.eatingwell.com/recipe/280556/grilled-flank-steak-corn-with-green-goddess-butter/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func mangoChicken(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "http://www.eatingwell.com/recipe/280555/mango-ginger-bbq-chicken/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func sweetChips(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "http://www.eatingwell.com/recipe/280639/melting-sweet-potatoes-with-herbs-garlic/'")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func niceCream(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "http://eatingwell.com/recipe/280638/pink-lemonade-nice-cream/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func creamsicle(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "http://www.eatingwell.com/recipe/280592/whipped-frozen-creamsicle/")! as URL, options: [:], completionHandler: nil)
+    }
+    
     
     
       override func viewDidLoad() {
